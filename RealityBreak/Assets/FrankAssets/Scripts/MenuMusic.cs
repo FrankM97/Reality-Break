a41fd107aeabc1ba3582b8ed14d43d3a804f133c
ArrowOmega <38959592+ArrowOmega@users.noreply.github.com>
2019-11-20 01:43:41 -0500

    Added Main Menu Music

---
+++ RealityBreak/Assets/FrankAssets/Scripts/MenuMusic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMusic : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("MenuMusic");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+        DontDestroyOnLoad(this);
+
+    }
+}
---
+++ RealityBreak/Assets/FrankAssets/Scripts/StopMenuMusic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StopMenuMusic : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject A = GameObject.FindGameObjectWithTag("MenuMusic");
+        Destroy(A);
+    }
+}
